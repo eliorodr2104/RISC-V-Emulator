14ff3de01f22cf488441ad1c11d25a4e3984e2c7
eliorodr2104 <rodriguezeliomar088@gmail.com>
2025-06-15 23:02:47 +0200

    Aggiunta della documentazione per le apposite funzioni e tutto il codice è stato adattato in underscore

--- src/alu/alu.c
+++ src/alu/alu.c
@@ -1,77 +1,100 @@
-//
-// Created by Eliomar Alejandro Rodriguez Ferrer on 02/06/25.
-//
+/**
+ * @file alu.h
+ * @brief Source file for the ALU operations
+ *
+ * @author eliorodr2104
+ * @date 02/06/25
+ *
+ * This file contains the function's arithmetic and logical operations in a 1-bit and 32-bit ALU.
+ */
 
 #include "alu.h"
 #include "tools.h"
 
-AluResult alu1bit(
-    bool a,
-    bool b,
-    const bool less,
-    const bool carryIn,
-    const uint8_t invertControl,
-    const uint8_t aluOp
+/**
+ * @brief Perform a 1-bit ALU operation
+ *
+ * @param a The first bit
+ * @param b The second bit
+ * @param less The less than bit (used for set less than operation), this is 0 for all bits except the last one
+ * @param carry_in The carry in bit for addition/subtraction
+ * @param invert_control Control bits to determine if inputs should be inverted
+ * @param alu_operation The ALU operation to perform
+ * @return AluResult containing the result and all flags
+ */
+AluResult alu_1_bit(
+          bool    a,
+          bool    b,
+    const bool    less,
+    const bool    carry_in,
+    const uint8_t invert_control,
+    const uint8_t alu_operation
 ) {
-    const bool binvert = getBit(invertControl, 3); //operation[1];
-    const bool ainvert = getBit(invertControl, 4); //operation[0];
+    // Get the bits, invert if necessary
+    const bool b_invert = getBit(invert_control, 3);
+    const bool a_invert = getBit(invert_control, 4);
 
-    AluResult result;
+    // Declared struct for result
+    AluResult result = {};
 
-    switch (aluOp) {
+    // Control the ALU operation
+    switch (alu_operation) {
 
         // Operation
-        // 0000 -> AND
-        // 1100 -> NOR
+        // 00000 -> AND
+        // 11000 -> NOR
         case 0:
 
-            if (ainvert) a = a ^ 1;
-            if (binvert) b = b ^ 1;
+            if (a_invert) a = a ^ 1;
+            if (b_invert) b = b ^ 1;
 
             result.result   = a & b;
-            result.carryOut = false;
+            result.carry_out = false;
 
             break;
 
         // Operation
-        // 0001 -> OR
+        // 00001 -> OR
         case 1:
 
             result.result   = a | b;
-            result.carryOut = false;
+            result.carry_out = false;
 
             break;
 
         // Operation
-        // 0010 -> ADD
-        // 0110 -> SUB
+        // 00010 -> ADD
+        // 01010 -> SUB
         case 2:
 
-            if (ainvert) a = a ^ 1;
-            if (binvert) b = b ^ 1;
+            if (a_invert) a = a ^ 1;
+            if (b_invert) b = b ^ 1;
 
-            result.result   = a ^ b ^ carryIn;
-            result.carryOut = (a & b) | (carryIn & (a ^ b));
+            result.result   = a ^ b ^ carry_in;
+            result.carry_out = (a & b) | (carry_in & (a ^ b));
 
             break;
         // Operation
-        // 0011 -> Set less than
+        // 00011 -> Set less than
         case 3:
             result.result   = less;
-            result.carryOut = false;
+            result.carry_out = false;
 
             break;
 
+        // Operation
+        // 00100 -> XOR
         case 7:
 
             result.result   = a ^ b;
-            result.carryOut = false;
+            result.carry_out = false;
 
             break;
 
+        // Default case for unrecognized operations
         default:
             result.result = false;
-            result.carryOut = false;
+            result.carry_out = false;
 
             break;
 
@@ -80,62 +103,98 @@ AluResult alu1bit(
     return result;
 }
 
-Alu32BitResult alu32bit(
+/**
+ * @brief Perform a 32-bit ALU operation
+ *
+ * @param a The first 32-bit integer
+ * @param b The second 32-bit integer
+ * @param less The less than bit (used for set less than operation), this is 0 for all bits except the last one
+ * @param operation The ALU operation to perform
+ * @return Alu32BitResult containing the result, zero flag, and overflow flag
+ */
+Alu32BitResult alu_32_bit(
     const int32_t a,
     const int32_t b,
     const bool less,
     const uint8_t operation
 ) {
-    Alu32BitResult alu32BitResult = { 0, 0, 0};
 
-    if (operation == 0xE) return alu32BitResult;
+    // Initialize the result structure
+    Alu32BitResult alu32_bit_result = { 0, 0, 0};
 
-          bool carryIn          = getBit(operation, 3);
-    const uint32_t aluOperation = (operation & 0b111); //bitsToInt(3, (bool[]){ operation[2], operation[3], operation[4] });
+    // If the operation is 0xE (Unrecognize operation), return the result immediately
+    if (operation == 0xE) return alu32_bit_result;
+
+    // Extract the carry in bit and the ALU operation from the operation code
+    bool carryIn = getBit(operation, 3);
+
+    // Extract the ALU operation bits (bits 2, 3, and 4)
+    const uint32_t alu_operation = (operation & 0b111);
 
     // Shift Left Arithmetic
-    if (aluOperation == 4) {
-        alu32BitResult.result = a << bitsToShiftAmount(b);
+    if (alu_operation == 4) {
+        alu32_bit_result.result = a << bits_to_shift_amount(b);
+
+        // Jump to CONTROL_ZERO to set zero and overflow flags
         goto CONTROL_ZERO;
     }
 
     // Shift Right Logical
-    if (aluOperation == 5) {
-        alu32BitResult.result = a >> bitsToShiftAmount(b);
+    if (alu_operation == 5) {
+        alu32_bit_result.result = a >> bits_to_shift_amount(b);
+
+        // Jump to CONTROL_ZERO to set zero and overflow flags
         goto CONTROL_ZERO;
     }
 
     // Shift Right Arithmetic
-    if (aluOperation == 6) {
-        const int shiftAmount = bitsToShiftAmount(b);
+    if (alu_operation == 6) {
+        const uint32_t shiftAmount = bits_to_shift_amount(b);
 
-        alu32BitResult.result = a >> shiftAmount;
+        alu32_bit_result.result = a >> shiftAmount;
+
+        // Jump to CONTROL_ZERO to set zero and overflow flags
         goto CONTROL_ZERO;
     }
 
-    int32_t resultBits = 0;
-    bool carryInOverflowControl  = 0;
-    bool carryOutOverflowControl = 0;
+    // Declared variables for the result bits, carry in and carry out
+    int32_t result_bits = 0;
+    bool carry_in_overflow_control  = 0;
+    bool carry_out_overflow_control = 0;
 
+    // For loop through each bit of the 32-bit integers and pass them to the 1-bit ALU operation
     for (int i = 0; i < 32; i++) {
-        const bool bitA      = getBit(a, i);
-        const bool bitB      = getBit(b, i);
-        const bool lessInput = (i == 31) ? less : false;
 
-        const AluResult res  = alu1bit(bitA, bitB, lessInput, carryIn, operation, aluOperation);
-        setBit(&resultBits, i, res.result);
+        // Get the bits from the integers a and b, and the less than bit if it's the last iteration
+        const bool bit_a      = getBit(a, i);
+        const bool bit_b      = getBit(b, i);
+        const bool less_input = (i == 31) ? less : false;
 
-        carryIn = res.carryOut;
+        // Get the ALU operation for this bit
+        const AluResult res  = alu_1_bit(bit_a, bit_b, less_input, carryIn, operation, alu_operation);
 
-        if (i == 1) carryInOverflowControl  = carryIn;
-        if (i == 0) carryOutOverflowControl = res.carryOut;
+        // Set the result bit in the result_bits variable
+        setBit(&result_bits, i, res.result);
+
+        // Update the carry in for the next iteration
+        carryIn = res.carry_out;
+
+        // Set the carry in and carry out overflow control bits
+        if (i == 1) carry_in_overflow_control  = carryIn;
+        if (i == 0) carry_out_overflow_control = res.carry_out;
     }
 
-    alu32BitResult.result = resultBits;
+    // Set the result in the alu32_bit_result structure
+    alu32_bit_result.result = result_bits;
 
+// This label is used to set the zero and overflow flags
 CONTROL_ZERO:
-    alu32BitResult.zero     = alu32BitResult.result == 0;
-    alu32BitResult.overflow = (aluOperation >= 4 && aluOperation <= 6 ? false : carryInOverflowControl ^ carryOutOverflowControl);
 
-    return alu32BitResult;
+    // Set the zero flag if the result is zero
+    alu32_bit_result.zero     = alu32_bit_result.result == 0;
+
+    // Set the overflow flag based on the ALU operation
+    alu32_bit_result.overflow = (alu_operation >= 4 && alu_operation <= 6 ? false : carry_in_overflow_control ^ carry_out_overflow_control);
+
+    return alu32_bit_result;
 }
--- src/alu/include/alu.h
+++ src/alu/include/alu.h
@@ -1,44 +1,93 @@
-//
-// Created by Eliomar Alejandro Rodriguez Ferrer on 02/06/25.
-//
+/**
+ * @file alu.h
+ * @brief Header file for the ALU operations
+ *
+ * @author eliorodr2104
+ * @date 02/06/25
+ *
+ * This file contains the definitions and function prototypes for performing
+ * arithmetic and logical operations in a 1-bit and 32-bit ALU.
+ */
 
 #ifndef ALU_H
 #define ALU_H
 #include <stdint.h>
 
+/**
+ * @brief Struct to hold the result of a 1-bit ALU operation
+ *
+ * @var AluResult::result
+ * The result of the ALU operation
+ * @var AluResult::carry_out
+ * The carry-out flag
+ */
 typedef struct {
     bool result;
-    bool carryOut;
+    bool carry_out;
+
 } AluResult;
 
+/**
+ * @brief Struct to hold the result of a 32-bit ALU operation
+ *
+ * @var Alu32BitResult::result
+ * The result of the ALU operation
+ * @var Alu32BitResult::zero
+ * The zero flag, set if the result is zero
+ * @var Alu32BitResult::overflow
+ * The overflow flag, set if there was an overflow in the operation
+ */
 typedef struct {
     int32_t result;
     bool zero;
     bool overflow;
+
 } Alu32BitResult;
 
-AluResult alu1bit (
-    bool a,
-    bool b,
-    bool less,
-    bool carryIn,
-    uint8_t invertControl,
-    uint8_t aluOp
+/**
+ * @brief Perform a 1-bit ALU operation
+ *
+ * @param a The first bit
+ * @param b The second bit
+ * @param less The less than bit (used for set less than operation), this is 0 for all bits except the last one
+ * @param carry_in The carry in bit for addition/subtraction
+ * @param invert_control Control bits to determine if inputs should be inverted
+ * @param alu_operation The ALU operation to perform
+ * @return AluResult containing the result and all flags
+ */
+AluResult alu_1_bit (
+    bool    a,
+    bool    b,
+    bool    less,
+    bool    carry_in,
+    uint8_t invert_control,
+    uint8_t alu_operation
 );
 
-Alu32BitResult alu32bit(
+/**
+ * @brief Perform a 32-bit ALU operation
+ *
+ * @param a The first 32-bit integer
+ * @param b The second 32-bit integer
+ * @param less The less than bit (used for set less than operation), this is 0 for all bits except the last one
+ * @param operation The ALU operation to perform
+ * @return Alu32BitResult containing the result, zero flag, and overflow flag
+ */
+Alu32BitResult alu_32_bit(
     int32_t a,
     int32_t b,
     bool less,
     uint8_t operation
 );
 
-static int bitsToShiftAmount(const int32_t b) {
+/**
+ * @brief Get the number of bits to shift based on the input value
+ *
+ * @param b The input value (should be in the range 0-31)
+ * @return The number of bits to shift (0-31)
+ */
+static uint32_t bits_to_shift_amount(const int32_t b) {
     return b & 0x1F;
 }
 
-static int twoBitsToDecimal(const bool a, const bool b) {
-    return (a << 1) | b;
-}
-
-#endif //ALU_H
+#endif
